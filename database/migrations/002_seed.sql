6a16f82018f20a44fa9a0798218a3cd18ac74fb0
Ana Stefan <anastefana28@gmail.com>
2025-06-14 18:02:01 +0300

    lala

--- database/migrations/002_seed.sql
+++ database/migrations/002_seed.sql
@@ -93,17 +93,6 @@ ON CONFLICT DO NOTHING;
 
 SET LOCAL media.base_path TO 'E:\Anul II\Semestrul II\Web\CaT_EchipaTeam\helpers\media';   
 
-
-CREATE OR REPLACE FUNCTION public.slugify(txt text)
-RETURNS text LANGUAGE sql IMMUTABLE AS $$
-  SELECT regexp_replace(
-           lower(translate(txt,
-             ' ĂÂÎȘŞȚŢăâîșşțţÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝŸàáâãäåçèéêëìíîïñòóôõöøùúûüýÿ',
-             ' aaiissttaaaiissttaaaceeeeiiiinoooooouuuuyyaaaiissttaaaceeeeiiiinoooooouuuuyy')),
-           '[^a-z0-9]+', '-', 'g');
-$$;
-
-
 DO $$
 DECLARE
   c     RECORD;
